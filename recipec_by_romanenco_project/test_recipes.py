82cd2bbac3c4769065269847dad4759bb3819856
Romanenco Taisia <tromanenco@edu.hse.ru>
2026-06-02 19:56:21 +0300

    Добавила 7 тестов для класса рецептов. Все они, слава Богу, прошли.Первый - на инициализацию. Второй- на добавление нового ингредтиента, третий- на добавление уже существующего , всё через функции описанные в ммэйне. Ттретий- на метод scale. Он проверил сразу несколько вещей.Он проверяет, что метод scale создает новый объект Recipe, не изменяет исходный рецепт и правильно умножает количество ингредиентов на коэффициент.Четвертый тоже на метод скейл. test_scale2: проверяет, что при попытке масштабировать рецепт с коэффициентом 0 метод scale выбрасывает ValueError.test_scale3: проверяет, что при отрицательном коэффициенте масштабирования метод scale выбрасывает ValueError.test_len: проверяет подсчет уникальных ингредиентов в рецепте.

--- recipec_by_romanenco_project/test_recipes.py
+++ recipec_by_romanenco_project/test_recipes.py
@@ -31,6 +31,73 @@ def test_eq_ingr3():
 
 
 
+"""testi for recipies"""
+
+
+def test_create():
+    recipe = Recipe("Mamaliga")
+    assert recipe.title == "Mamaliga"
+    assert recipe.ingredients == []
+
+def test_add_ingredient():
+    recipe = Recipe("Mamaliga")
+    flour = Ingredient("corn flour", 500, "г")
+    recipe.add_ingredient(flour)
+    assert len(recipe.ingredients) == 1
+    assert recipe.ingredients[0].name == "corn flour"
+    assert recipe.ingredients[0].quantity == 500
+    assert recipe.ingredients[0].unit == "г"
+
+
+def test_add_same_ingredient():
+    recipe = Recipe("Льняная каша")
+
+    flour1 = Ingredient("Мука льняная", 50, "г")
+    flour2 = Ingredient("Мука льняная", 20, "г")
+    recipe.add_ingredient(flour1)
+    recipe.add_ingredient(flour2)
+    
+    assert len(recipe.ingredients) == 1
+    assert recipe.ingredients[0].name == "Мука льняная"
+    assert recipe.ingredients[0].quantity == 70
+    assert recipe.ingredients[0].unit == "г"
+
+
+def test_scale():
+    recipe = Recipe("Яичница по-молдавски")
+    recipe.add_ingredient(Ingredient("Яйца",2, "шт"))
+    recipe.add_ingredient(Ingredient("Творог", 200, "г"))
+    scaled_recipe = recipe.scale(2)
+
+    assert scaled_recipe is not recipe
+    assert isinstance(scaled_recipe, Recipe)
+    assert recipe.ingredients[0].quantity == 2
+    assert recipe.ingredients[1].quantity == 200
+    assert scaled_recipe.ingredients[0].quantity == 4
+    assert scaled_recipe.ingredients[1].quantity == 400
+
+
+def test_scale2():
+    recipe = Recipe("Орешки с медом")
+    recipe.add_ingredient(Ingredient("Орешки", 10, "кг"))
+    with pytest.raises(ValueError):
+        recipe.scale(0)
+
+
+def test_scale3():
+    recipe = Recipe("Орешки с медом")
+    recipe.add_ingredient(Ingredient("Орешки", 10, "кг"))
+    with pytest.raises(ValueError):
+        recipe.scale(-10)
+
+
+def test_len():
+    recipe = Recipe("Орешки с медом")
+    recipe.add_ingredient(Ingredient("Орешки", 500, "г"))
+    recipe.add_ingredient(Ingredient("Мед", 20, "г"))
+    recipe.add_ingredient(Ingredient("Орешки", 100, "г"))
+    
+    assert len(recipe) == 2
     
            
          
